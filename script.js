2d0dd580fda123c22138d3309fea6807e9938372
Decodedal <Palumbodallas@gmail.com>
2022-07-29 15:22:32 -0400

    losrt dogogg

--- script.js
+++ script.js
@@ -9,20 +9,54 @@ canvas.width = 612;
 canvas.height = 612;
 
 ctx.drawImage(myImage,0,0, canvas.width, canvas.height);
+const pixles = ctx.getImageData(0, 0, canvas.width, canvas.height);
+ctx.clearRect(0,0,canvas.width, canvas.height);
+
 
 let particlesArray = [];
 const numberOfParticles = 5000;
+
+let mappedImage =[];
+for(let y = 0; y<canvas.height; y++){
+    let row = [];
+    for(let x = 0; x < canvas.width; x++){
+        const red =  pixles.data[(y *4 * pixles.width)+(x*4)];
+        const green = pixles.data[(y *4 * pixles.width)+(x*4 + 1)];
+        const blue = pixles.data[(y *4 * pixles.width)+(x*4 + 2)];
+        const brightness = calculateRelativeBrightness(red,green,blue)
+        const cell = [
+            cellBrightness =brightness 
+        ];
+        row.push(cell);
+    }
+    mappedImage.push(row);
+}
+function calculateRelativeBrightness(red,green,blue){
+    return Math.sqrt(
+    (red *red) * 0.299 +
+    (green *green) * 0.587 +
+    (blue * blue) * 0.114
+    )/100;
+}
+
 
 class Particle{
     constructor(){
         this.x = Math.random()* canvas.width;
         this.y = 0;
         this.speed = 0; 
-        this.velocity = Math.random()* 3.5;
+        this.velocity = Math.random()* 0.5;
         this.size = Math.random() * 1.5 + 1;   
+        this.position1 = Math.floor(this.y);
+        this.position2 = Math.floor(this.x);
     }
     update(){
-        this.y += this.velocity;
+        this.position1 = Math.floor(this.y);
+        this.position2 = Math.floor(this.x);
+        this.speed = mappedImage[this.position1][this.position2][0];
+        let movement = (2.5 - this.speed) + this.velocity;
+
+        this.y += movement;
         if(this.y >= canvas.height){
             this.y = 0;
             this.x = Math.random() * canvas.width;
@@ -30,7 +64,7 @@ class Particle{
     }
     draw(){
         ctx.beginPath();
-        ctx.fillStyle = 'white'
+        ctx.fillStyle = 'red'
         ctx.arc(this.x, this.y, this.size, 0 , Math.PI * 2);
         ctx.fill();
     }
@@ -42,14 +76,16 @@ function init(){
 }
 init();
 function animate(){
-    ctx.drawImage(myImage,0,0, canvas.width, canvas.height);
+    // ctx.drawImage(myImage,0,0, canvas.width, canvas.height);
 ctx.globalAlpha = 0.05;
 ctx.fillStyle = 'rgb(0,0,0)';
-ctx.fillRect(0,0,canvas.width, canvas.height)
-particlesArray.forEach(a=>{
-    a.update();
-    a.draw()
-})
+ctx.fillRect(0,0,canvas.width, canvas.height);
+ctx.globalAlpha = 0.2
+for(let i = 0; i< particlesArray.length; i++){
+    particlesArray[i].update()
+    ctx.globalAlpha = particlesArray[i].speed *0.2;
+    particlesArray[i].draw();
+}
 requestAnimationFrame(animate);
 }
 animate()
